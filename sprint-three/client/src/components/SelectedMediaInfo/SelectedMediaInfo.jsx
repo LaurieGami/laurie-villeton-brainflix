468cb10e64c96a327832e406cab4aeb772b27e9f
LaurieGami <laurievilleton@gmail.com>
2021-06-24 18:44:02 -0700

    Added the like button functionality

--- sprint-three/client/src/components/SelectedMediaInfo/SelectedMediaInfo.jsx
+++ sprint-three/client/src/components/SelectedMediaInfo/SelectedMediaInfo.jsx
@@ -9,36 +9,47 @@ import likesIcon from '../../assets/icons/Icon-likes.svg';
 import Comments from "../Comments/Comments";
 import { timeAgo } from '../../utils/timeAgo';
 
-function SelectedMediaInfo ({selectedEntry, getVideoDetails}) {
-    return (
-        <article className="selected-media">
-            <section className="selected-media__info">
-                <h1 className="selected-media__title">{selectedEntry.title}</h1>
-                <div className="selected-media__subtitles">
-                    <div className="selected-media__top">
-                        <h2 className="selected-media__channel">By {selectedEntry.channel}</h2>
-                        <p className="selected-media__timestamp">{timeAgo(selectedEntry.timestamp)}</p>
-                    </div>
-                    <div className="selected-media__bottom">
-                        <div className="selected-media__stats">
-                            <img className="selected-media__icon" src={viewsIcon} alt="Views Icon"/>
-                            <p className="selected-media__number">{selectedEntry.views}</p>
+class SelectedMediaInfo extends Component {
+
+    addLike = () => {
+        axios.put(`/videos/${this.props.selectedEntry.id}/likes`)
+        .then(_res => this.props.getVideoDetails(this.props.selectedEntry.id))
+        .catch(err => console.log('Oops', err))
+    }
+    
+    render() {
+        const {selectedEntry, getVideoDetails} = this.props;
+
+        return (
+            <article className="selected-media">
+                <section className="selected-media__info">
+                    <h1 className="selected-media__title">{selectedEntry.title}</h1>
+                    <div className="selected-media__subtitles">
+                        <div className="selected-media__top">
+                            <h2 className="selected-media__channel">By {selectedEntry.channel}</h2>
+                            <p className="selected-media__timestamp">{timeAgo(selectedEntry.timestamp)}</p>
                         </div>
-                        <div className="selected-media__stats">
-                            <img className="selected-media__icon" src={likesIcon} alt="Likes Icon"/>
-                            <p className="selected-media__number">{selectedEntry.likes}</p>
+                        <div className="selected-media__bottom">
+                            <div className="selected-media__stats">
+                                <img className="selected-media__icon" src={viewsIcon} alt="Views Icon"/>
+                                <p className="selected-media__number">{selectedEntry.views}</p>
+                            </div>
+                            <div className="selected-media__stats">
+                                <img onClick={this.addLike} className="selected-media__icon selected-media__icon--like" src={likesIcon} alt="Likes Icon"/>
+                                <p className="selected-media__number">{selectedEntry.likes}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <p className="selected-media__description">
-                    {selectedEntry.description}
-                </p>
-            </section>
-            <section className="selected-media__comments">
-                <Comments selectedEntry={selectedEntry} getVideoDetails={getVideoDetails}/>
-            </section>
-        </article>
-    )
+                    <p className="selected-media__description">
+                        {selectedEntry.description}
+                    </p>
+                </section>
+                <section className="selected-media__comments">
+                    <Comments selectedEntry={selectedEntry} getVideoDetails={getVideoDetails}/>
+                </section>
+            </article>
+        )
+    }
 }
 
 export default SelectedMediaInfo;
